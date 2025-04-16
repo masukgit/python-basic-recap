b2a6193145c5cc8c8ef2ab0e86a33253ba8c5c05
masukgit <masuk1111@gmail.com>
2025-04-16 23:08:44 +0600

    copied

---
+++ variable.py
@@ -0,0 +1,20 @@
+x = 2
+name = 'Masuk'
+ismale = True
+
+print(ismale)
+
+print(type(x))
+
+x = None
+print(x)
+print(type(x))
+
+p= 8.61
+n = int(p)
+print(n)
+
+m_string = '29.35'
+m_float = float(m_string)
+mn_string = str(m_float)
+print(mn_string)
